904af626d470c7d49599c6ea5e9d295dc15aee20
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-08-22 13:22:51 +0300

    Add project files.

---
+++ AjaxProject/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using AjaxProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AjaxProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly Context _context;
+
+        public DefaultController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProductList()
+        {
+            var values = _context.Products.ToList();
+            var jsonValues= JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+    }
+}
